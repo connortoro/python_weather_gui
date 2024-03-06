f6ce79d48a0d0a7d81cc9ff92ee3b73c804684b5
connor <connor.toro@gmail.com>
2024-03-06 13:00:19 -0800

    added hashing/salting to passwords

--- user_db.py
+++ user_db.py
@@ -1,5 +1,6 @@
 import os
 import pymongo
+import bcrypt
 from dotenv import load_dotenv, find_dotenv
 
 # Load env variables
@@ -10,26 +11,32 @@ pwd = os.environ.get("DB_PWD")
 # Connect to mongodb
 uri = f"mongodb+srv://{username}:{pwd}@cluster0.yuchdcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = pymongo.MongoClient(uri)
-user_db = client.user.user
+user_db = client.user.credentials
 
-def insert_user(_username, _password):
+def insert_user(username, password):
+
+    # Hash Password
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     # Create user
     user = {
-        "username": _username,
-        "password": _password
+        "username": username,
+        "hash": hashed_pwd,
+        "salt": salt
     }
 
     # Insert user
     user_db.insert_one(user)
 
 
-def check_password(_username, _password):
-    user = user_db.find_one({"username": _username})
+def check_password(username, password):
+    user = user_db.find_one({"username": username})
     if user:
-        password = user.get("password")
-        if _password == password:
-            return True
+        hash = user.get("hash")
+        salt = user.get("salt")
+        new_hash = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return new_hash == hash
 
     return False
 
